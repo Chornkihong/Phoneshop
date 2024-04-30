e6601a4c324b8c4bc0679a1df84e95540877555d
John kihong <kihongchorn@gmail.com>
2024-04-30 18:58:53 +0700

    JPA Specification and Pagination

--- src/main/java/com/kihong/learn/java/phoneshop/service/impl/BrandServiceImpl.java
+++ src/main/java/com/kihong/learn/java/phoneshop/service/impl/BrandServiceImpl.java
@@ -1,50 +1,110 @@
 package com.kihong.learn.java.phoneshop.service.impl;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
 import com.kihong.learn.java.phoneshop.entity.Brand;
-import com.kihong.learn.java.phoneshop.exception.ApiException;
 import com.kihong.learn.java.phoneshop.exception.ResourceNotFoundException;
 import com.kihong.learn.java.phoneshop.repository.BrandRepository;
 import com.kihong.learn.java.phoneshop.service.BrandService;
+import com.kihong.learn.java.phoneshop.service.Spec.BrandFilter;
+import com.kihong.learn.java.phoneshop.service.Spec.BrandSpec;
+import com.kihong.learn.java.phoneshop.service.util.PageUtil;
 
 @Service
 public class BrandServiceImpl implements BrandService {
-     @Autowired
+	@Autowired
 	private BrandRepository brandRepository;
+
 	@Override
 	public Brand Create(Brand brand) {
 		return brandRepository.save(brand);
 	}
+
 	@Override
 	public Brand getById(Integer id) {
 		// approach 1
-	     //Optional<Brand> brandOptional = brandRepository.findById(id);
-	     //if(brandOptional.isPresent()) {
-	    	 //return brandOptional.get();
-	     //}
-		//throw new  HttpClientErrorException(HttpStatus.NOT_FOUND,"Brand with id="+id+" not found"); //or
-		//throw new  HttpClientErrorException(HttpStatus.NOT_FOUND,"Brand with id=%d not found".formatted(id)); //or
-	     //throw new  HttpClientErrorException(HttpStatus.NOT_FOUND,String.format("Brand with id=%d not found", id));
-	    //end approach 1
-		
-		//approach 2
+		// Optional<Brand> brandOptional = brandRepository.findById(id);
+		// if(brandOptional.isPresent()) {
+		// return brandOptional.get();
+		// }
+		// throw new HttpClientErrorException(HttpStatus.NOT_FOUND,"Brand with id="+id+"
+		// not found"); //or
+		// throw new HttpClientErrorException(HttpStatus.NOT_FOUND,"Brand with id=%d not
+		// found".formatted(id)); //or
+		// throw new HttpClientErrorException(HttpStatus.NOT_FOUND,String.format("Brand
+		// with id=%d not found", id));
+		// end approach 1
+
+		// approach 2
 		return brandRepository.findById(id)
-				//.orElseThrow(()-> new HttpClientErrorException(HttpStatus.NOT_FOUND,String.format("Brand with id=%d not found", id)));
-	    .orElseThrow(()->new ResourceNotFoundException("Brand",id) );
+				// .orElseThrow(()-> new
+				// HttpClientErrorException(HttpStatus.NOT_FOUND,String.format("Brand with id=%d
+				// not found", id)));
+				.orElseThrow(() -> new ResourceNotFoundException("Brand", id));
 	}
+
 	@Override
 	public Brand Update(Integer id, Brand brandUpdate) {
-		 Brand brand = getById(id);
-		  brand.setName(brandUpdate.getName()); //@TODO Improve update
+		Brand brand = getById(id);
+		brand.setName(brandUpdate.getName()); // @TODO Improve update
 		return brandRepository.save(brand);
 	}
-	
-	
+
+//	@Override
+//	public List<Brand> getBrand() {
+//		return brandRepository.findAll();
+//	}
+	// V1
+	/*
+	 * @Override public List<Brand> getBrand(String name) { return
+	 * brandRepository.findByNameContaining(name); }
+	 * 
+	 * @Override public List<Brand> getBrand(Map<String, String> params) {
+	 * BrandFilter brandFilter = new BrandFilter(); if (params.containsKey("name"))
+	 * { String name = params.get("name"); brandFilter.setName(name); } if
+	 * (params.containsKey("id")) { String id = params.get("id");
+	 * brandFilter.setId(Integer.parseInt(id)); }
+	 * 
+	 * BrandSpec brandSpec = new BrandSpec(brandFilter); return
+	 * brandRepository.findAll(brandSpec); }
+	 */
+	// end V1
+
+	@Override
+	public List<Brand> getBrand(String name) {
+		return brandRepository.findByNameContaining(name);
+	}
+
+	@Override
+	public Page<Brand> getBrand(Map<String, String> params) {
+		BrandFilter brandFilter = new BrandFilter();
+		if (params.containsKey("name")) {
+			String name = params.get("name");
+			brandFilter.setName(name);
+		}
+		if (params.containsKey("id")) {
+			String id = params.get("id");
+			brandFilter.setId(Integer.parseInt(id));
+		}
+		//TODO add a function for pageable
+		int pageLimit =PageUtil.DEFAULT_PAGE_LIMIT;
+		if (params.containsKey(PageUtil.PAGE_LIMIT)) {
+			pageLimit = Integer.parseInt(params.get(PageUtil.PAGE_LIMIT));
+		}
+		int pageNumber = 1;
+		if (params.containsKey(PageUtil.PAGE_NUMBER)) {
+			pageNumber = Integer.parseInt(params.get(PageUtil.PAGE_NUMBER));
+		}
+		BrandSpec brandSpec = new BrandSpec(brandFilter);
+		Pageable pageable = PageUtil.getPageable(pageNumber, pageLimit);
+		Page<Brand> page = brandRepository.findAll(brandSpec, pageable);
+		return page;
+	}
 
 }
